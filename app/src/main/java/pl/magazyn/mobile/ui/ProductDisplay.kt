c9427f64ea085b034fec788b0d968a813e18c736
Thekwspl <206255548+Thekwspl@users.noreply.github.com>
2026-09-07 23:30:36 +0200

    bug fixes

--- app/src/main/java/pl/magazyn/mobile/ui/ProductDisplay.kt
+++ app/src/main/java/pl/magazyn/mobile/ui/ProductDisplay.kt
@@ -1,6 +1,7 @@
 package pl.magazyn.mobile.ui
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
