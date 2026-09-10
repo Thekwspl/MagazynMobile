3ee7c851c6ad972782cd79c4a7317db1a7c467db
Thekwspl <206255548+Thekwspl@users.noreply.github.com>
2026-09-10 23:05:32 +0200

    v0.9.30 FIX

--- app/src/main/java/pl/magazyn/mobile/ui/OrdersScreen.kt
+++ app/src/main/java/pl/magazyn/mobile/ui/OrdersScreen.kt
@@ -1,5 +1,6 @@
 package pl.magazyn.mobile.ui
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
